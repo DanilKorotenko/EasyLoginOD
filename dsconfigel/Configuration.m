7270ecee0db95151c71eacae0839fa016369caed
Yoann Gini <yoann.gini@gmail.com>
2017-06-05 23:29:11 +0200

    First move to an ObjC based command line with behavior and name close to builtin command line

--- dsconfigel/Configuration.m
+++ dsconfigel/Configuration.m
@@ -8,11 +8,9 @@
 
 #import "Configuration.h"
 #import <OpenDirectory/OpenDirectory.h>
+#import <Cocoa/Cocoa.h>
 
-#define kConfigurationModuleName @"EasyLogin"
-#define kConfigurationServiceName @"io.easylogin.EasyLoginOD"
-
-#define kConfigurationExampleServer @"acme.easylogin.cloud"
+#import "Common.h"
 
 @interface Configuration ()
 
@@ -23,7 +21,6 @@
 
 @property NSString *host;
 @property NSInteger port;
-
 
 @property NSInteger queryTimeout;
 @property NSInteger idleTimeout;
@@ -47,7 +44,6 @@
         _action = [[NSUserDefaults standardUserDefaults] stringForKey:@"action"];
         _host = [[NSUserDefaults standardUserDefaults] stringForKey:@"host"];
         _port = [[NSUserDefaults standardUserDefaults] integerForKey:@"port"];
-        
         
         _queryTimeout = [[NSUserDefaults standardUserDefaults] integerForKey:@"queryTimeout"];
         _idleTimeout = [[NSUserDefaults standardUserDefaults] integerForKey:@"idleTimeout"];
@@ -83,7 +79,7 @@
 }
 
 - (NSString*)nodeMountPath {
-    return [NSString stringWithFormat:@"/EasyLogin/%@", self.host];
+    return [NSString stringWithFormat:@"/EasyLogin"];
 }
 
 - (int)checkCommonRequierements {
@@ -96,9 +92,6 @@
         return EXIT_FAILURE;
     }
     
-    
-
-    
     if (![@"show" isEqualToString:self.action]) {
         if ([self.host length] == 0) {
             fprintf(stderr, "You must specify a target host to run this action\n");
@@ -107,7 +100,6 @@
             
             return EXIT_FAILURE;
         }
-        
         
         self.odSession = [ODSession defaultSession];
         
@@ -146,103 +138,30 @@
     ODMappings *odMaps = [ODMappings mappings];
     odConfig.defaultMappings = odMaps;
     
-    NSArray *mapList = @[
-                         
-                         // Users
-                         @{
-                             @"EL_native_type": @"CloudUser",
-                             @"EL_std_type": kODRecordTypeUsers,
-                             @"EL_attribute_mapping": @{
-                                     kODAttributeTypeRecordType: @"type",
-                                     
-                                     kODAttributeTypeGUID: @"uuid",
-                                     kODAttributeTypeUniqueID: @"uid",
-                                     kODAttributeTypePrimaryGroupID: @"primarygroup", // should be hard linked to staff for macOS
-                                     
-                                     kODAttributeTypeRecordName: @"shortname",
-                                     kODAttributeTypeComment: @"description",
-                                     kODAttributeTypeCreationTimestamp: @"creationdate", // YYYYMMDDHHMMSSZ
-                                     kODAttributeTypeModificationTimestamp: @"modificationdate", // YYYYMMDDHHMMSSZ
-                                     
-                                     kODAttributeTypeFullName: @"displayname",
-                                     kODAttributeTypeLastName: @"lastname",
-                                     kODAttributeTypeFirstName: @"firstname",
-                                     kODAttributeTypeMiddleName: @"middlename",
-                                     kODAttributeTypeNamePrefix: @"prefix",
-                                     kODAttributeTypeNickName: @"nickname",
-                                     kODAttributeTypeBirthday: @"birthday",
-                                     kODAttributeTypeEMailAddress: @"email",
-                                     kODAttributeTypeJPEGPhoto: @"photo",
-                                     kODAttributeTypeAuthenticationHint: @"passwordhint",
-                                     kODAttributeTypeWeblogURI: @"blog",
-                                     
-                                     kODAttributeTypeFaxNumber: @"fax",
-                                     kODAttributeTypePhoneNumber: @"phone",
-                                     kODAttributeTypeMobileNumber: @"mobilephone",
-                                     kODAttributeTypeHomePhoneNumber: @"personalphone",
-                                     kODAttributeTypeIMHandle: @"instantmessaging",
-                                     kODAttributeTypePagerNumber: @"pager",
-                                     
-                                     kODAttributeTypeAddressLine1: @"addresline1",
-                                     kODAttributeTypeAddressLine2: @"addresline2",
-                                     kODAttributeTypeAddressLine3: @"addresline3",
-                                     kODAttributeTypeState: @"state",
-                                     kODAttributeTypeStreet: @"street",
-                                     kODAttributeTypeAreaCode: @"areacode",
-                                     kODAttributeTypeBuilding: @"building",
-                                     kODAttributeTypeCity: @"city",
-                                     kODAttributeTypeCompany: @"company",
-                                     kODAttributeTypeCountry: @"country",
-                                     kODAttributeTypeDepartment: @"department",
-                                     kODAttributeTypeJobTitle: @"jobtitle",
-                                     kODAttributeTypeOrganizationInfo: @"organizationinfo",
-                                     kODAttributeTypeOrganizationName: @"organizationname",
-                                     
-                                     kODAttributeTypeUserSMIMECertificate: @"publicmailcert",
-                                     kODAttributeTypeUserCertificate: @"internalcert",
-                                     kODAttributeTypeUserPKCS12Data: @"pkcs12",
-                                     kODAttributeTypePGPPublicKey: @"pgppublickey",
-                                     
-                                     kODAttributeTypeUserShell: @"shell",
-                                     kODAttributeTypeHomeDirectory: @"home",
-                                     kODAttributeTypeHomeDirectoryQuota: @"homehardquota",
-                                     kODAttributeTypeHomeDirectorySoftQuota: @"homesoftquota",
-                                     },
-                             },
-                         
-                         
-                         // Groups
-                         @{
-                             @"EL_native_type": @"CloudGroup",
-                             @"EL_std_type": kODRecordTypeGroups,
-                             @"EL_attribute_mapping": @{
-                                     kODAttributeTypeFullName: @"common_name",
-                                     kODAttributeTypeRecordName: @"shortname",
-                                     kODAttributeTypeGUID: @"uuid",
-                                     },
-                             },
-                         
-                         ];
+    NSString *odModulePath = [NSString stringWithFormat:@"/Library/OpenDirectory/Modules/%@.xpc/Contents/Resources/ELMapping.plist", kEasyLoginODModuleIdentifier];
     
-    for (NSDictionary *mapInfo in mapList) {
+    NSDictionary *mappingInfos = [NSDictionary dictionaryWithContentsOfFile:odModulePath];
+    
+    for (NSString *nativeType in [mappingInfos allKeys]) {
+        NSDictionary *mapInfos = [mappingInfos objectForKey:nativeType];
+        NSString *standardType = [mapInfos objectForKey:kEasyLoginMappingInfosStandardTypeKey];
+        
         ODRecordMap *recordMap = [ODRecordMap recordMap];
-        recordMap.native = [mapInfo objectForKey:@"EL_native_type"];
+        recordMap.native = nativeType;
         
-        NSDictionary *attributeMap = [mapInfo objectForKey:@"EL_attribute_mapping"];
+        NSDictionary *standardToNativeAttributesMap = [mapInfos objectForKey:kEasyLoginMappingInfosStandardToNativeKey];
         
-        for (NSString *key in [attributeMap allKeys]) {
-            [recordMap setAttributeMap:[ODAttributeMap attributeMapWithValue:[attributeMap objectForKey:key]] forStandardAttribute:key];
+        for (NSString *standardAttributeType in [standardToNativeAttributesMap allKeys]) {
+            [recordMap setAttributeMap:[ODAttributeMap attributeMapWithValue:[standardToNativeAttributesMap objectForKey:standardAttributeType]] forStandardAttribute:standardAttributeType];
         }
         
-        [odMaps setRecordMap:recordMap forStandardRecordType:[mapInfo objectForKey:@"EL_std_type"]];
+        [odMaps setRecordMap:recordMap forStandardRecordType:standardType];
     }
     
-    
-    
-    ODModuleEntry *odModuleEntry = [ODModuleEntry moduleEntryWithName:kConfigurationModuleName xpcServiceName:kConfigurationServiceName];
-    
+    ODModuleEntry *odModuleEntry = [ODModuleEntry moduleEntryWithName:kEasyLoginODModuleName xpcServiceName:kEasyLoginODModuleIdentifier];
+        
     if (!odModuleEntry) {
-        fprintf(stderr, "EasyLogin were unable to load new instance of OD Module %s. Check content of OD Module folder.\n", [kConfigurationServiceName UTF8String]);
+        fprintf(stderr, "EasyLogin were unable to load new instance of OD Module %s. Check content of OD Module folder.\n", [kEasyLoginODModuleIdentifier UTF8String]);
         return EXIT_FAILURE;
     }
     
@@ -290,23 +209,24 @@
     fprintf(stderr, "\tDisplay current configuration\n");
     fprintf(output, "\n");
     
-    fprintf(stderr, "%s -action join -host %s ...\n", command, [kConfigurationExampleServer UTF8String]);
+    fprintf(stderr, "%s -action join -host %s ...\n", command, [kEasyLoginSampleServer UTF8String]);
     fprintf(stderr, "\tBind to target EasyLogin server.\n");
     fprintf(stderr, "\tIf an authentication is requiered by the server,\n\tclient certificate will be looked in system keychain.\n");
     fprintf(output, "\n");
     
-    fprintf(stderr, "%s -action leave -host %s ...\n", command, [kConfigurationExampleServer UTF8String]);
+    fprintf(stderr, "%s -action leave -host %s ...\n", command, [kEasyLoginSampleServer UTF8String]);
     fprintf(stderr, "\tLeave the target server\n");
     fprintf(stderr, "\tIf server was using certificate based authentication,\n\tcertificate will stay in system keychain.\n");
     fprintf(output, "\n");
     
-    fprintf(stderr, "%s -action update -host %s ...\n", command, [kConfigurationExampleServer UTF8String]);
+    fprintf(stderr, "%s -action update -host %s ...\n", command, [kEasyLoginSampleServer UTF8String]);
     fprintf(stderr, "\tUpdate current configuration settings\n");
     fprintf(output, "\n");
     
     fprintf(output, "Defaults values for optional arguments in current context are:\n");
     
     NSArray *keys = @[@"port", @"queryTimeout", @"idleTimeout", @"setupTimeout"];
+    
     int padLength = 20;
     
     for (NSString *key in keys) {

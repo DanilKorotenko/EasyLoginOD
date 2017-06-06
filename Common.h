e8f41ad9f0aa35be30c8e3948a8f89a0f65c489c
Yoann Gini <yoann.gini@gmail.com>
2017-06-06 10:31:19 +0200

    Missing file

---
+++ Common.h
@@ -0,0 +1,21 @@
+//
+//  Common.h
+//  EasyLoginOD
+//
+//  Created by Yoann Gini on 02/06/2017.
+//  Copyright © 2017 Yoann Gini (Open Source Project). All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+#define kEasyLoginODModuleEntryName @"EasyLogin"
+#define kEasyLoginODModuleIdentifier @"io.easylogin.EasyLoginOD"
+#define kEasyLoginODModuleBundleName @"io.easylogin.EasyLoginOD"
+
+#define kEasyLoginSampleServer @"acme.easylogin.cloud"
+
+#define kEasyLoginMappingInfosStandardTypeKey @"standardType"
+#define kEasyLoginMappingInfosStandardToNativeKey @"standardToNativeMap"
+
+#endif /* Common_h */
